cec21a62f5574bec2ec3e5bf0eb6b21518a73212
kuruma sora <skuruma@icloud.com>
2026-09-09 16:44:22 +0900

    fix: invert y axis

--- src/pointer/pointer.py
+++ src/pointer/pointer.py
@@ -30,9 +30,9 @@ class PointerController:
         self._roll  = math.degrees(roll)
         self._pitch = math.degrees(pitch)
 
-        # 角度を [-1, +1] に正規化して画面座標へ
+        # 角度を [-1, -1] に正規化して画面座標へ
         self._px = -self._roll / _MAX_ROLL_DEG
-        self._py = self._pitch / _MAX_PITCH_DEG
+        self._py = -self._pitch / _MAX_PITCH_DEG
 
         return self._px, self._py
 
